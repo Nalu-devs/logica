f2860bf297b58e3e02df0ccf8c9cdc3f1b963c71
alunoEtec <aula@etec.com>
2025-04-07 20:30:26 -0300

    definindo numero de casas depois da virgula

---
+++ Linguagem C/area.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main(){
+	float l, c, ar; /*Declaração de variavel do tipo real*/
+	char nome[40], op;/*Declaração de variavel do tipo caracter, grande exemplo: nome e um caracter exemplo:op*/
+	printf("Digite a largura do terreno: ");
+	scanf("%f",&l);
+	printf("Digite o comprimento do terreno: ");
+	scanf("%f",&c);
+	ar=l*c;
+	printf("A area do seu terreno de largura: %.2f e comprimento: %.2f é igual a: %.2f",l, c, ar);/*O comando .numero define quantas casas vai ter depois da virgula*/
+}
