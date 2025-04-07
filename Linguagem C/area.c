98074ff718248ff1b5f003ce53d839560dda9e75
alunoEtec <aula@etec.com>
2025-04-07 20:28:01 -0300

    comentarios

--- Linguagem C/area.c
+++ Linguagem C/area.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 main(){
-	float l, c, ar;
-	char nome[40], op;
+	float l, c, ar; /*Declaração de variavel do tipo real*/
+	char nome[40], op;/*Declaração de variavel do tipo caracter, grande exemplo: nome e um caracter exemplo:op*/
 	printf("Digite a largura do terreno: ");
 	scanf("%f",&l);
 	printf("Digite o comprimento do terreno: ");
