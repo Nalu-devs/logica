44776c439a169205828d19d6f4596f1ae29cdf6f
ana <ana.lucia.cavalcante.sirino@gmail.com>
2025-05-28 11:16:34 -0300

    comecando o exercicio 25

---
+++ Linguagem C/025.c
@@ -0,0 +1,10 @@
+//Escrever um programa que receba vários números inteiros no teclado. E
+//no final imprimir a média dos números múltiplos de 3. Para sair digitar 0
+//(zero)
+
+#include <stdio.h>
+#include <locale.h>
+main(){
+    setlocale(LC_ALL, "portuguese");
+    
+}
