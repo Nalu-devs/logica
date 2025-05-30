091b758c3868fc00301920e236e95128d6989116
alunoEtec <aula@etec.com>
2025-05-29 21:59:22 -0300

    exercicio 28 em C

---
+++ Linguagem C++/037.c++
@@ -0,0 +1,9 @@
+//Guardar em um vetor o ano de nascimento de 10 pessoas e gerar em outro
+//vetor a idade de cada pessoa. Mostrar como resultado o ano de nascimento
+//e a idade calculada das 10 pessoas e mostre qual a média das idades.
+
+#include <iostream>
+using namespace std;
+main(){
+    char nome[10] [30];//o 10 é para repetir 10 vezes e o 30 é a quantidade de caracter
+}
---
+++ Linguagem C/028.c
@@ -0,0 +1,17 @@
+//Programa que calcule a divisão de um numero pelo outro, sabendo que o quociente tem que ser diferente de 0
+
+#include <stdio.h>
+#include <locale.h>
+main(){
+    setlocale(LC_ALL,"portuguese");
+    float num1, num2, divsao;
+    printf("Digite um numero:\n\n");
+    scanf("%f",&num1);
+    printf("Digite outro numero:\n\n");
+    do{
+    scanf("%f",&num2);
+    printf("Digite um numero diferente de 0\n");
+    }while(num==0);
+    divisao=num1/num2;
+    printf("A divisão de %2.f e %2.f é igual a %2.f", num1, num2, divisao);
+}
