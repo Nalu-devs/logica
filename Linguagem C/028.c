bd4c148424a19a0a03bca145532c74ce59ab5177
alunoEtec <aula@etec.com>
2025-05-29 22:32:16 -0300

    ajustando exercicio 28 em c

--- Linguagem C/028.c
+++ Linguagem C/028.c
@@ -4,14 +4,14 @@
 #include <locale.h>
 main(){
     setlocale(LC_ALL,"portuguese");
-    float num1, num2, divsao;
+    float num1, num2, divisao;
     printf("Digite um numero:\n\n");
     scanf("%f",&num1);
     printf("Digite outro numero:\n\n");
     do{
     scanf("%f",&num2);
     printf("Digite um numero diferente de 0\n");
-    }while(num==0);
+    }while(num2<=0);
     divisao=num1/num2;
     printf("A divisão de %2.f e %2.f é igual a %2.f", num1, num2, divisao);
 }
